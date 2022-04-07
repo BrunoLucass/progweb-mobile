5ccc8436141629e2640f563efd7b5560d73212e4
Bruno Lucas <70291159+BrunoLucass@users.noreply.github.com>
2022-04-07 19:02:40 -0300

    ex04 completo

--- ex04/main.js
+++ ex04/main.js
@@ -1,17 +1,52 @@
-function obterElemento () {
-  let elemento = document.getElementById('elemento').value
+function criaDiv() {
+  if(document.querySelector('#item').value===""){
+    alert("Insira um nome para seu contador");
 
-  // valida 
-
-  if (elemento === '') {
-    alert('Preencha o campo com uma palavra')
+}else{
+    const itemName = document.getElementById('item').value;
+    const item = document.createElement('div');
+    item.style.backgroundColor = randomColor();
+    item.classList.add('item-count');
+    item.innerHTML = 
+    `
+    <p class="item-name">${itemName}</p> 
+    <output id="count">0</output>
+    <br>
+    <br>  
+    <button onclick="reset(event)" class="buttonReset">Zerar</button>
+    `;
+    item.onclick = (event) => {count(event)}
+    document.getElementById('item-section').appendChild(item);
+    limpaInput();
   }
-  let elementoObtido
-  elemento = elementoObtido
-
-
-  var elementos = document.getElementById(elementoObtido).value
-
-  let outputElemento = document.getElementById('outputElemento')
-  outputElemento.value = elementoObtido
+}
+  function count(event){
+    console.log(event);
+    console.log(event.target);
+    if(event.target.classList[0] == 'item-count'){
+        const item = event.target;
+        //const count = item.querySelector('.count');
+        //item.getElementById('count');
+        const count = item.children[1];
+        count.innerHTML = parseInt(count.innerHTML) + 1;
+    } else{
+        const item = event.target.parentElement;
+        const count = item.children[1];
+        count.innerHTML = parseInt(count.innerHTML) + 1;
+    }
+    
+}
+function reset(event){
+    const item = event.target.parentElement;
+    const count = item.children[1];
+    count.innerHTML = '-1';
+}
+function randomColor()
+{
+  return '#' + parseInt((Math.random() * 0xFFFFFF))
+    .toString(16)
+    .padStart(6, '0');
+}
+function limpaInput(){
+    document.getElementById('item').value = "";
 }
